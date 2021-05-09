f5bf8f3e0ae75dea36152573e13c525fb66b602f
mikeisfake <mikescooper@icloud.com>
2021-05-08 22:31:19 -0400

    create header component and modify about page to use it

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../components/header'
 
 const About = () => {
   return (
     <div style={{color: `teal`}} >
-      <h1>About Gatsby!!!!!</h1>
+      <Header headerText='About Gatsby!' />
       <p>such wow. very react</p>
     </div>
   )
