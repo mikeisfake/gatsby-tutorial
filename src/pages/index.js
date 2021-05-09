0d7286d7a72baeb0dcbf0c0aee572df5fbc418be
mikeisfake <mikescooper@icloud.com>
2021-05-08 22:12:58 -0400

    refactor example as arrow function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+// export default function Home() {
+//   return <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+// }
+
+const Home = () => {
+  return <div style={{ color: `purple`, fontSize: `72px` }} >
+    Helo MIke!!!
+  </div>
 }
+
+export default Home
