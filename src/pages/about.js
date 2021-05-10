0751ce5a54a5e0f2f7a53877b0dd45b2e261436f
mikeisfake <mikescooper@icloud.com>
2021-05-10 16:02:57 -0400

    add style sheet and gatsby browser file

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,14 +13,3 @@ const About = () => {
 
 export default About
 
-
-// import React from "react"
-//
-// export default function About() {
-//   return (
-//     <div style={{ color: `teal` }}>
-//       <h1>About Gatsby</h1>
-//       <p>Such wow. Very React.</p>
-//     </div>
-//   )
-// }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,17 @@
 import React from "react"
-import { Link } from 'gatsby'
+import {Link} from 'gatsby'
 import Header from '../components/header'
 
-// export default function Home() {
-//   return <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
-// }
-
 const Home = () => {
-  return(
-    <div style={{ color: `purple` }}>
-      <Link to='/contact'>Contact</Link>
-      <Header headerText="Hi Mike" />
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
-  )
+  return (<div style={{
+      color: `purple`,
+      fontSize: `72`
+  }}>
+    <Link to='/contact'>Contact</Link>
+    <Header headerText="Hi Mike" anotherProp='valueHere' propThree="wootowwt"/>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt=""/>
+  </div>)
 }
 
 export default Home
